8b51ffba77f13590e03e142f40b60ba5db01714b
Russell Love <loru9001@GBLC02XH5WHJG5M.local>
2019-08-20 10:15:20 +0100

    Update MarksSelection.js

--- MarksSelection.js
+++ MarksSelection.js
@@ -144,7 +144,7 @@
 
     var x = d3.scaleLinear()
         .domain([0, d3.max(barValues)])
-        .range([0, width]);
+        .range([0, width+30]);
 
     // Add scales to axis
     var x_axis = d3.axisBottom().scale(x);
@@ -166,7 +166,7 @@
         .attr("height", barHeight - 1);
     
     bar.append("text")
-        .attr("x", function(d) { return x(d) - 3; })
+        .attr("x", function(d) { return x(d) + 5; })
         .attr("y", barHeight / 2)
         .attr("dy", ".35em")
         .text(function(d) { return d;});
