ee134de1ac4a6ee66a6ce9dab9726d86a3df7f53
Russell Love <loru9001@GBLC02XH5WHJG5M.local>
2019-08-20 10:23:16 +0100

    Update MarksSelection.js

--- MarksSelection.js
+++ MarksSelection.js
@@ -166,7 +166,7 @@
         .attr("height", barHeight - 1);
     
     bar.append("text")
-        .attr("x", function(d) { return x(d) - 3; })
+        .attr("x", function(d) { return x(d) + 3; })
         .attr("y", barHeight / 2)
         .attr("dy", ".35em")
         .text(function(d) { return d;});
