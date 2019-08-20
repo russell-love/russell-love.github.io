0ffae073c913c0508d4f2079499ad2f9a0727500
Russell Love <loru9001@GBLC02XH5WHJG5M.local>
2019-08-20 08:21:47 +0100

    Update MarksSelection.js

--- MarksSelection.js
+++ MarksSelection.js
@@ -158,13 +158,6 @@
         .enter().append("g")
         .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
 
-    // Add scales to axis
-    var x_axis = d3.axisBottom().scale(scale);
-
-    //Append group and insert axis
-    bar.append("g").call(x_axis);
-
-
     bar.append("rect")
         .attr("width", x)
         .attr("height", barHeight - 1);
