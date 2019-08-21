5754e1f97f362ee8d248a4fb24723d66417027c6
Russell Love <loru9001@GBLC02XH5WHJG5M.local>
2019-08-21 09:11:33 +0100

    Update MarksSelection.js

--- MarksSelection.js
+++ MarksSelection.js
@@ -213,7 +213,7 @@
 
     var x = d3.scaleLinear()
         .domain([0, d3.max(data, d => d.recordcount)])
-        .range([0, width]);
+        .range([0, width + 50]);
 
     // Add scales to axis
     var x_axis = d3.axisBottom().scale(x);
