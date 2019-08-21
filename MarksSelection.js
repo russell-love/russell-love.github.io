dd69fc690486f68d233ad51740d9e5b6d3b21be5
Russell Love <loru9001@GBLC02XH5WHJG5M.local>
2019-08-21 08:54:51 +0100

    Update MarksSelection.js

--- MarksSelection.js
+++ MarksSelection.js
@@ -284,11 +284,7 @@
       }
     })
 
-    mappedData.sort(function(a, b) {
-      if (a[2] < b[2]) return  -1;
-      if (a[2] > b[2]) return 1;    
-      return 0;
-    });
+    var mappedData = arry.sort(function(a,b){ return a[2] > b[2] ? 1 : -1; });
 
     console.log(mappedData);
 
