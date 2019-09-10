7f6d00b35599e6e4136e025a7c34869afda35812
Russell Love <loru9001@GBLC02XH5WHJG5M.local>
2019-09-10 14:54:34 +0100

    Update Horizbar.js

--- Horizbar.js
+++ Horizbar.js
@@ -75,12 +75,13 @@
         $('#data_table_wrapper').empty();
         $('#data_table_wrapper').append("Got sheet name");
 
+        /*
         sheet.getUnderlyingDataAsync(options).then(function (t) {
             var data = t.getData(); 
             $('#data_table_wrapper').empty();
             $('#data_table_wrapper').append("Got underlying Data");                               
         });
-
+        */
     }
 
 /*
