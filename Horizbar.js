5c26edd6742dbcd6591551e88ac8d523dd71b500
Russell Love <loru9001@GBLC02XH5WHJG5M.local>
2019-09-11 09:58:09 +0100

    Update Horizbar.js

--- Horizbar.js
+++ Horizbar.js
@@ -74,15 +74,13 @@
             //var data = t.data;
             $('#data_table_wrapper').empty();
             $('#data_table_wrapper').append("Got underlying Data");
-            console.log(t.columns[1]);
-            /*
+
             var data = t
-                .columns( [0, 1] )
+                .columns[0, 1]
                 .data()
                 .flatten();
-            */
-            
-                                          
+                           
+            console.log(data);               
         });
         
     }
