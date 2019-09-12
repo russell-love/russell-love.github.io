28af4c580d56e96ad27139fc01b85d8bff02a361
Russell Love <loru9001@GBLC02XH5WHJG5M.local>
2019-09-12 15:50:55 +0100

    Update Horizbar.js

--- Horizbar.js
+++ Horizbar.js
@@ -175,7 +175,7 @@
 
             // X Scale
             var x = d3.scaleLinear()
-                .domain(0, [d3.max(revenueByMonth, function(d) { return d.value.totalRevenue })])
+                .domain([0, d3.max(revenueByMonth, function(d) { return d.value.totalRevenue })])
                 .range([height, 0]);
 
 
