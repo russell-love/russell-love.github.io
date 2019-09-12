6450ef9dcf85a4b787e841c81f9926cbc2fa67ea
Russell Love <loru9001@GBLC02XH5WHJG5M.local>
2019-09-12 09:34:42 +0100

    Update Horizbar.js

--- Horizbar.js
+++ Horizbar.js
@@ -60,152 +60,57 @@
     }
 
     function loadSelectedMarks(worksheetName) {
-        /*
-        $('#data_table_wrapper').empty();
-        $('#data_table_wrapper').append("Starting");
-
-        //sheet = viz.getWorkbook().getActiveSheet();
-        const sheet = getSelectedSheet(worksheetName);
-        $('#data_table_wrapper').empty();
-        $('#data_table_wrapper').append("Got sheet name");
-
-        
-        sheet.getUnderlyingDataAsync().then(function (t) {
-            console.log(typeof t);
-            console.log(t)
-;
-            $('#data_table_wrapper').empty();
-            $('#data_table_wrapper').append("Got underlying Data");
-        });
-        
-         // If the active sheet is not a dashboard, then you can just enter:
-         // viz.getWorkbook().getActiveSheet();
-        const options = {
-            maxRows: 10, // Max rows to return. Use 0 to return all rows
-            ignoreAliases: false,
-            ignoreSelection: true,
-            includeAllColumns: false
-        };
-
-        sheet.getUnderlyingDataAsync(options).then(function(t){
-               var table = t;
-               console.log(t);
-               console.log(JSON.stringify(t.getData()));
-           });
-        */
-
+        //Setup variables
         var viz, sheet, options;
 
+        //Set the desired sheet to the selected sheet
         sheet = getSelectedSheet(worksheetName);
-         // If the active sheet is not a dashboard, then you can just enter:
-         // viz.getWorkbook().getActiveSheet();
+
+        //Set options for the getUnderlyingData call
         options = {
-            maxRows: 10, // Max rows to return. Use 0 to return all rows
+            maxRows: 0, // Max rows to return. Use 0 to return all rows
             ignoreAliases: false,
             ignoreSelection: true,
             includeAllColumns: false
         };
 
+        //getUnderlyingData call
         sheet.getUnderlyingDataAsync(options).then(function(t){
-           showData_2(t);
+           cleanData(t); //Call the cleanData function (maps and converts)
         });      
     }
 
-    function showData_2(t) {
+    function cleanData(t) {
         console.log(t);
 
-        var data = t.data;
-        var dataCols = t.columns;
+        //Set variables to pass to conversion function
+        var data = t.data; //Data
+        var dataCols = t.columns; //Column names
         
         console.log(dataCols);
         console.log(data);
         
-        var niceData = reduceToObjects(dataCols, data);
+        var niceData = reduceToObjects(dataCols, data); //conversion call
 
         console.log(niceData);
     }
 
     //convert to field:values convention
     function reduceToObjects(cols,data) {
-      var fieldNameMap = $.map(cols, function(col) { return col.fieldName; });
+        //Map column names from column data
+        var fieldNameMap = $.map(cols, function(col) { return col.fieldName; });
 
-      var dataToReturn = $.map(data, function(d) {
-        return d.reduce(function(memo, value, idx) {
-          memo[fieldNameMap[idx]] = value.formattedValue; return memo;
-        }, {});
-      });
-      return dataToReturn;
+        //Map the column names to the data
+        var dataToReturn = $.map(data, function(d) {
+            return d.reduce(function(memo, value, idx) {
+            memo[fieldNameMap[idx]] = value.formattedValue; return memo;
+            }, {});
+        });
+
+        return dataToReturn;
       
-
     }
     
-/*
-        // Set our title to an appropriate value
-        $('#selected_marks_title').text(worksheet.name);
-
-        // Call to get the selected marks for our sheet
-        worksheet.getSelectedMarksAsync().then(function(marks) {
-            // Get the first DataTable for our selected marks (usually there is just one)
-            const worksheetData = marks.data[0];
-
-            // Map our data into the format which the data table component expects it
-            const data = worksheetData.data.map(function(row, index) {
-                const rowData = row.map(function(cell) {
-                    return cell.formattedValue;
-                });
-
-                return rowData;
-            });
-
-            const columns = worksheetData.columns.map(function(column) {
-                return {
-                    title: column.fieldName
-                };
-            });
-
-            // Populate the data table with the rows and columns we just pulled out
-            populateDataTable(data, columns);
-        });
-        
-*/
-
-    function showData(data) {
-
-        $('#data_table_wrapper').empty();
-
-        $('#data_table_wrapper').append(data);
-        $('#data_table_wrapper').append("Done");
-    }
-
-    function populateDataTable(data, columns) {
-        // Do some UI setup here to change the visible section and reinitialize the table
-        $('#data_table_wrapper').empty();
-
-        if (data.length > 0) {
-            $('#no_data_message').css('display', 'none');
-            $('#data_table_wrapper').append(`<table id='data_table' class='table table-striped table-bordered'></table>`);
-
-            // Do some math to compute the height we want the data table to be
-            var top = $('#data_table_wrapper')[0].getBoundingClientRect().top;
-            var height = $(document).height() - top - 130;
-
-            // Initialize our data table with what we just gathered
-            $('#data_table').DataTable({
-                data: data,
-                columns: columns,
-                autoWidth: false,
-                deferRender: true,
-                scroller: true,
-                scrollY: height,
-                scrollX: true,
-                dom: "<'row'<'col-sm-6'i><'col-sm-6'f>><'row'<'col-sm-12'tr>>" // Do some custom styling
-            });
-        } else {
-            // If we didn't get any rows back, there must be no marks selected
-            $('#no_data_message').css('display', 'inline');
-        }
-    }
-
     function initializeButtons() {
         $('#show_choose_sheet_button').click(showChooseSheetDialog);
     }
