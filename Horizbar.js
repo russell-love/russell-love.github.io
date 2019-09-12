13a2deb93dc2e072e96d8269ad4580e2da9ded74
Russell Love <loru9001@GBLC02XH5WHJG5M.local>
2019-09-12 14:27:28 +0100

    Update Horizbar.js

--- Horizbar.js
+++ Horizbar.js
@@ -162,11 +162,8 @@
             var revenueByMonth = d3.nest()
                 .key(function(d) { return d.Month; })
                 .rollup(function(f) {
-                    console.log(f[0]);
-
                     return { 
                         totalRevenue: d3.sum(f, function(g) { return g.Revenue; }), 
-                        dataMonth: f, function(g) { return g.Month; }
                     }
                 })
                 .entries(data);
