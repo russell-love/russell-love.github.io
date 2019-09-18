e343aa377cd7ef8dfb031654ac6d5ad67ba9f645
Russell Love <loru9001@GBLC02XH5WHJG5M.local>
2019-09-18 15:55:05 +0100

    Update Horizbar.js

--- Horizbar.js
+++ Horizbar.js
@@ -205,7 +205,7 @@
                     .attr("class","label")
                     .attr("x", 30)
                     .attr("y", function(d){ return y(tParser(d.key)) })
-                    .attr("dy", "2.0em")
+                    .attr("dy", "3.0em")
                     .text(function(d){ return formattedLabelText(d.value.totalRevenue); });
     }       
 })();
