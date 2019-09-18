6d1abfd424f823c8e562fb1ef62601774d707f6e
Russell Love <loru9001@GBLC02XH5WHJG5M.local>
2019-09-18 15:42:35 +0100

    Update Horizbar.js

--- Horizbar.js
+++ Horizbar.js
@@ -197,7 +197,7 @@
                     .attr("width", function(d){ return x(d.value.totalRevenue); })
                     .attr("fill", "purple");
 
-            var formattedLabelText = d3.format("$.2s");
+            var formattedLabelText = d3.format("$,");
             g.selectAll(".text")          
                 .data(revenueByMonth)
                 .enter()
