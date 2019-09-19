d0135503cdfca2d1f05b8726da85ffe61911f8fa
Russell Love <loru9001@GBLC02XH5WHJG5M.local>
2019-09-19 15:51:46 +0100

    Update Vertbar.js

--- Vertbar.js
+++ Vertbar.js
@@ -24,7 +24,7 @@
             dashboard.objects.forEach(function (object) {
                 // do something with the objects..
                 //console.log("The object name is " + object.name)
-                if (object.name == "HorizBarTest") {
+                if (object.name == "VertBarTest") {
                     console.log("Width : " + object.size.width);
                     console.log("Height : " + object.size.height);
 
