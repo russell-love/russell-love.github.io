d33bb2fba4d6eca0ef9648fb60b96d3a6b40a23f
Russell Love <loru9001@GBLC02XH5WHJG5M.local>
2019-09-20 11:22:05 +0100

    Update Line_bar.js

--- Line_bar.js
+++ Line_bar.js
@@ -206,6 +206,8 @@
                 .y(function(d){ return height - y(d.value.totalSuccessful); }) // set the y values for the line generator 
                 .curve(d3.curveMonotoneX) // apply smoothing to the line
 
+            console.log(line);
+            
             g.append("path")
                 .datum(successPercByMonth) // 10. Binds data to the line 
                 .attr("class", "line") // Assign a class for styling 
