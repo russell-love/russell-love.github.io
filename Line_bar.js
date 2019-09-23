a9752d2f69f61914a0099ec0470f8aff6c6ae060
Russell Love <loru9001@GBLC02XH5WHJG5M.local>
2019-09-23 11:16:30 +0100

    Update Line_bar.js

--- Line_bar.js
+++ Line_bar.js
@@ -236,14 +236,6 @@
                     .attr("fill", "orange")
                     .attr("r", 3.5);
 
-            g.append("g").selectAll("text")
-                .data(successPercByMonth)
-                    .enter().append("text")
-                        .attr("x", function(d, i) { return x(tParser(d.key)) + (x.bandwidth() / 2) - 10; })
-                        .attr("y", function(d) { return y2(d.value.successRate) - 10; })
-                        .attr("fill", "black")
-                        .text(function(d) { return formattedLabelPerc(d.value.successRate / 100); });
-
             g.append("g").selectAll("rect")
                 .data(successPercByMonth)
                 .enter().append("rect")
@@ -251,7 +243,15 @@
                     .attr("y", function(d) { return y2(d.value.successRate) - 20; })
                     .attr("width", 20)
                     .attr("height", 20)
-                    .attr("fill", "black");
+                    .attr("fill", "grey");
+
+            g.append("g").selectAll("text")
+                .data(successPercByMonth)
+                    .enter().append("text")
+                        .attr("x", function(d, i) { return x(tParser(d.key)) + (x.bandwidth() / 2) - 10; })
+                        .attr("y", function(d) { return y2(d.value.successRate) - 10; })
+                        .attr("fill", "black")
+                        .text(function(d) { return formattedLabelPerc(d.value.successRate / 100); });
 
     }       
 })();
