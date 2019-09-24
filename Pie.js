96e20db2839ba33d06ec0106b4d8c4b3f73ac34e
Russell Love <loru9001@GBLC02XH5WHJG5M.local>
2019-09-24 11:26:10 +0100

    Update Pie.js

--- Pie.js
+++ Pie.js
@@ -69,10 +69,10 @@
                     var p_value = p[i].currentValue; // This is DataValue object
                     
                     console.log(p_value);
-                    
+
                     //var p_actual_value = p.currentValue.value;
                     //var p_formatted_value = p.currentValue.formattedValue;
-                    console.log('Parameter ' + p_name + ' has the value ' + p_formatted_value);
+                    //console.log('Parameter ' + p_name + ' has the value ' + p_formatted_value);
                 }
             });
 
