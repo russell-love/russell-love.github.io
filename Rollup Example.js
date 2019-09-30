b94d0da8019924048e0680c042fae0a533cc591b
Russell Love <loru9001@GBLC02XH5WHJG5M.local>
2019-09-30 15:29:29 +0100

    Update Rollup Example.js

--- Rollup Example.js
+++ Rollup Example.js
@@ -177,9 +177,7 @@
             // Normally we'll distribute the colors
             // in the scale to child nodes.
             var colors;
-            colors = d3.scaleOrdinal(d3.schemeCategory10);
 
-            /*
             // The root node is special since
             // we have to seed it with our
             // desired palette.
@@ -210,7 +208,7 @@
                             d.children[child.idx].color = colors(i);
                 });
             }
-            */
+            
             return d.color;
         };
 
