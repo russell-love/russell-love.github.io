e6942a266f1e2be8928577d62655f2b60dc0ef10
Russell Love <loru9001@GBLC02XH5WHJG5M.local>
2019-09-30 11:14:42 +0100

    Update Rollup Example.js

--- Rollup Example.js
+++ Rollup Example.js
@@ -182,7 +182,7 @@
                     .attr("display", function (d) { return d.depth ? null : "none"; })
                     .attr("d", arc)
                     .style('stroke', '#fff')
-                    .style("fill", function (d) { return color((d.children ? d : d.parent).data["0"]); });
+                    .style("fill", function (d) { return color((d.children ? d : d.parent).data.data[0]); });
 
     }       
 })();
